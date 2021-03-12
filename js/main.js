9cc7888164c146330e733571e1907f58effe9722
DanielJordanov01 <daniel159696@gmail.com>
2021-03-12 11:14:51 +0000

    effectiveness section

--- js/main.js
+++ js/main.js
@@ -23,8 +23,8 @@ new Glider(document.querySelector('.glider'), {
           // screens greater than >= 1024px
           breakpoint: 1024,
           settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
+            slidesToShow: 3,
+            slidesToScroll: 3,
             itemWidth: 150,
             duration: 0.25
           }
