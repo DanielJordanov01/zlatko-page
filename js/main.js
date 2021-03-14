1ec664aaf1a0e4f93de308ff54949dc74b226c87
DanielJordanov01 <daniel159696@gmail.com>
2021-03-14 14:33:45 +0000

    typewritter effect

--- js/main.js
+++ js/main.js
@@ -50,3 +50,17 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+var i = 0;
+var txt = 'Dropshipping Mastery'; /* The text */
+var speed = 70; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+typeWriter();
